d3764a557bc1e8133fa9fed7aa94d772dd078466
SengKueVang <82859778+Sengkue@users.noreply.github.com>
2025-10-07 14:38:43 +0700

    Auto commit Tue-10-07 02-38

--- push.bat
+++ push.bat
@@ -1,16 +1,13 @@
 @echo off
-REM Automate git add, commit, and push
-
-REM Stage all changes
+REM Add all changes
 git add .
 
-REM Prompt for commit message
-set /p msg="Enter commit message: "
+REM Get current date and time
+for /f "tokens=1-4 delims=/ " %%a in ('date /t') do set mydate=%%a-%%b-%%c
+for /f "tokens=1-2 delims=: " %%a in ('time /t') do set mytime=%%a-%%b
 
-REM Commit changes
-git commit -m "%msg%"
+REM Commit with current date and time as message
+git commit -m "Auto commit %mydate% %mytime%"
 
-REM Push to current branch
+REM Push to remote
 git push
-
-pause
